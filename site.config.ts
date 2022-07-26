50b87a73ea63d998fc07660253cbd5cb97d9de07
Wido Wicaksono <widowicak@gmail.com>
2022-07-26 17:03:28 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Wido Wicaksono',
+  domain: 'wicaksono.xyz',
+  author: 'Wido Wicaksono',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Wido Wicaksono Portofolio',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'widowicaksono',
+  github: 'widcha',
+  linkedin: 'widowicaksono',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
